a5405e3eb9629e0dc57af26bcc8af4bf9a849d14
Bandana7 <112367587+Bandana7@users.noreply.github.com>
2024-10-07 09:57:22 +0100

    week3

---
+++ week3.java
@@ -0,0 +1,18 @@
+package week3;
+
+public class Stack { no usages
+
+    private int[] array; 1 usage
+    private int size; 1 usage
+
+    public Stack(int capacity) { no usages
+        array = new int[capacity];
+        size = 0;
+    }
+
+public void push (int element) { no usages
+    }
+ public int pop() { no usages
+    }
+    return 0;
+}
